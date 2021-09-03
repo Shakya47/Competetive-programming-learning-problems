16078035c06485f09a98d29a198b7c3117464c4d
Saurabh Shakya <42512860+Shakya47@users.noreply.github.com>
2021-09-03 11:12:22 +0530

    Create 02. Count setBits.cpp

---
+++ Bit Manupulation/02. Count setBits.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int countBits(int n){  //Time Complexity -> O(logn)
+  int ans = 0;
+  while(n>0){
+    ans += n&1;
+    n = n>>1;
+  }
+  return ans;
+}
+
+int countBitsFast(int n){  //Time Complexity -> O(no. of setbits)
+
+  int ans = 0;
+  while(n>0){
+    n = n & (n-1);
+    ans++;          //ans = no. of times the loop will run
+  }
+
+  return ans;
+
+}
+
+
+int main(){
+ 
+ int n ;
+ cin>>n;
+ cout<<countBits(n)<<endl;
+ cout<<countBitsFast(n)<<endl;
+ cout<<__builtin_popcount(n)<<endl; //inbuilt function
+  return 0;
+} 

f1b2e9089831d8c8433a0945724a7bbabf1692c5
Saurabh Shakya <42512860+Shakya47@users.noreply.github.com>
2021-09-13 19:30:48 +0530

    Create 04.Smallest Prime Factor for n numbers.cpp

---
+++ Prime Numbers/04.Smallest Prime Factor for n numbers.cpp
@@ -0,0 +1,24 @@
+//Time Complexity = O(nloglogn)
+//Based on Sieve Eratosthenes
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+ int n;
+ cin>>n;
+ 
+ vector<int> isPrime(n+1,0);
+
+ for(int i = 2; i<=n ; i++){        
+    if(isPrime[i]==0){
+      cout<<i<<" ";
+      for(int j=i*i; j<=n; j = j+i)   
+        isPrime[j]=i;
+    }
+    else  cout<<isPrime[i]<<" ";  
+  }
+
+}
+  
+

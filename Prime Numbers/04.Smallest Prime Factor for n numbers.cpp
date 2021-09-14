574034f6a943cad5d5269235d767d312967ccc01
Saurabh Shakya <42512860+Shakya47@users.noreply.github.com>
2021-09-14 10:24:54 +0530

    Update 04.Smallest Prime Factor for n numbers.cpp

--- Prime Numbers/04.Smallest Prime Factor for n numbers.cpp
+++ Prime Numbers/04.Smallest Prime Factor for n numbers.cpp
@@ -13,7 +13,8 @@ int main(){
  for(int i = 2; i<=n ; i++){        
     if(isPrime[i]==0){
       cout<<i<<" ";
-      for(int j=i*i; j<=n; j = j+i)   
+      for(int j=i*i; j<=n; j = j+i)  
+       if(isPrime[j]==0)  //Very important, for multiples of 30 it was showing wrong
         isPrime[j]=i;
     }
     else  cout<<isPrime[i]<<" ";  
